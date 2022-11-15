c3a24ea770a5e249883da5bf065b08b0d417ceb5
Abdulrahman Amir <64253660+3bdulra7manAmir@users.noreply.github.com>
2022-11-15 19:57:03 +0200

    Login_Page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,17 +1,70 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget
-{
+class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
-  Widget build(BuildContext context)
-  {
-    return Scaffold
-    (
-      body: Column
-      (
-        children: const [Text("data")], //Text in!
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          //padding of all sides
+          padding: const EdgeInsets.all(33.0), //padding value of all edges
+          child: Column(
+            children: [
+              const SizedBox(
+                //to push the textfield of the top of the screen
+                height: 64,
+              ),
+              TextField(
+                  keyboardType: TextInputType
+                      .emailAddress, //keyboard type either email or password or date etc
+                  obscureText: true, //**** stars password
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Email : ",
+                    // To delete borders
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: Divider.createBorderSide(context),
+                    ),
+                    focusedBorder: const OutlineInputBorder(
+                      //on click border color
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    // fillColor: Colors.red,
+                    filled: true, //inside color of the text field default color
+                    contentPadding: const EdgeInsets.all(
+                        8), //padding inside of the textfiled
+                  )),
+
+              const SizedBox(
+                height: 64,
+              ), //to push the textfield of the top of the screen
+
+              TextField(
+                  keyboardType: TextInputType
+                      .emailAddress, //keyboard type either email or password or date etc
+                  obscureText: true, //**** stars password
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Email : ",
+// To delete borders
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: Divider.createBorderSide(context),
+                    ),
+                    focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                      ),
+                    ), //on click border color
+// fillColor: Colors.red,
+                    filled: true, //inside color of the text field default color
+                    contentPadding: const EdgeInsets.all(
+                        8), //padding inside of the textfiled
+                  ))
+            ], //Text in!
+          ),
+        ),
       ),
     );
   }
