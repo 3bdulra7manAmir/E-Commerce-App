c0b43c70e23bf9e5455f9f1781a292dc31ae4c76
Abdulrahman Amir <64253660+3bdulra7manAmir@users.noreply.github.com>
2022-11-15 21:29:09 +0200

    added register page

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:taskn3/pages/login.dart';
+import 'package:taskn3/pages/register.dart';
 
-void main()
-{
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context)
-  {
-    return const MaterialApp
-    (
-      home: Login(), //Temp name untill we create our MainPage or in this case or Login Page
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home:
+          Register(), //Temp name untill we create our MainPage or in this case or Login Page
       debugShowCheckedModeBanner: false,
     );
   }
