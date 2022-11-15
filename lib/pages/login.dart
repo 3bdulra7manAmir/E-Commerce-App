6d66c0d32143385224e803992e3cf68610d1ba2c
Abdulrahman Amir <64253660+3bdulra7manAmir@users.noreply.github.com>
2022-11-15 20:56:30 +0200

    AbdulrahmanAmir Login Page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../shared/custom_textfield.dart';
+
 class Login extends StatelessWidget {
   const Login({super.key});
 
@@ -11,57 +13,26 @@ class Login extends StatelessWidget {
           //padding of all sides
           padding: const EdgeInsets.all(33.0), //padding value of all edges
           child: Column(
+            // ignore: prefer_const_literals_to_create_immutables
             children: [
               const SizedBox(
                 //to push the textfield of the top of the screen
                 height: 64,
               ),
-              TextField(
-                  keyboardType: TextInputType
-                      .emailAddress, //keyboard type either email or password or date etc
-                  obscureText: true, //**** stars password
-                  decoration: InputDecoration(
-                    hintText: "Enter Your Email : ",
-                    // To delete borders
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: Divider.createBorderSide(context),
-                    ),
-                    focusedBorder: const OutlineInputBorder(
-                      //on click border color
-                      borderSide: BorderSide(
-                        color: Colors.grey,
-                      ),
-                    ),
-                    // fillColor: Colors.red,
-                    filled: true, //inside color of the text field default color
-                    contentPadding: const EdgeInsets.all(
-                        8), //padding inside of the textfiled
-                  )),
+              MyTextField(
+                textInputTypee: TextInputType.emailAddress,
+                isPassword: false,
+                hinttextt: "Enter Your Email : ",
+              ),
 
               const SizedBox(
                 height: 64,
               ), //to push the textfield of the top of the screen
 
-              TextField(
-                  keyboardType: TextInputType
-                      .emailAddress, //keyboard type either email or password or date etc
-                  obscureText: true, //**** stars password
-                  decoration: InputDecoration(
-                    hintText: "Enter Your Password : ",
-// To delete borders
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: Divider.createBorderSide(context),
-                    ),
-                    focusedBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey,
-                      ),
-                    ), //on click border color
-// fillColor: Colors.red,
-                    filled: true, //inside color of the text field default color
-                    contentPadding: const EdgeInsets.all(
-                        8), //padding inside of the textfiled
-                  ))
+              MyTextField(
+                textInputTypee: TextInputType.text,
+                isPassword: true,
+                hinttextt: "Enter Your Password : ",),
             ], //Text in!
           ),
         ),
